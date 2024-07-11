b55139172420ed7edb75c6d5656c9d277d522485
Cosmin Ciausu <cciausu97@gmail.com>
2024-07-11 13:56:03 -0400

    Update run.py

--- mhubio/run.py
+++ mhubio/run.py
@@ -368,7 +368,7 @@ def run(config_file: Optional[str] = None, config_data: Optional[dict] = None):
     global import_paths
 
     print("CONFIG FILE...")
-    print(config_file))
+    print(config_file)
     # scan local modules
     import_paths = {**import_paths, **scan_local_modules()}
 
